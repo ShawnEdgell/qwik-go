445490c996275cec6d4cda6dc987566a5b1c1111
Shawn Edgell <milky.irl@gmail.com>
2024-05-30 16:58:18 -0400

    updated build config, added vercel config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -30,21 +30,18 @@ export default defineConfig(({ command, mode }): UserConfig => {
     },
 
     /**
-     * This is an advanced setting. It improves the bundling of your server code. To use it, make sure you understand when your consumed packages are dependencies or dev depencies. (otherwise things will break in production)
+     * This is an advanced setting. It improves the bundling of your server code. To use it, make sure you understand when your consumed packages are dependencies or dev dependencies. (otherwise things will break in production)
      */
-    // ssr:
-    //   command === "build" && mode === "production"
-    //     ? {
-    //         // All dev dependencies should be bundled in the server build
-    //         noExternal: Object.keys(devDependencies),
-    //         // Anything marked as a dependency will not be bundled
-    //         // These should only be production binary deps (including deps of deps), CLI deps, and their module graph
-    //         // If a dep-of-dep needs to be external, add it here
-    //         // For example, if something uses `bcrypt` but you don't have it as a dep, you can write
-    //         // external: [...Object.keys(dependencies), 'bcrypt']
-    //         external: Object.keys(dependencies),
-    //       }
-    //     : undefined,
+    ssr: {
+      // All dev dependencies should be bundled in the server build
+      noExternal: Object.keys(devDependencies),
+      // Anything marked as a dependency will not be bundled
+      // These should only be production binary deps (including deps of deps), CLI deps, and their module graph
+      // If a dep-of-dep needs to be external, add it here
+      // For example, if something uses `bcrypt` but you don't have it as a dep, you can write
+      // external: [...Object.keys(dependencies), 'bcrypt']
+      external: Object.keys(dependencies),
+    },
 
     server: {
       headers: {
@@ -58,6 +55,11 @@ export default defineConfig(({ command, mode }): UserConfig => {
         "Cache-Control": "public, max-age=600",
       },
     },
+    build: {
+      outDir: 'dist',
+      target: 'esnext',
+      ssrManifest: true
+    }
   };
 });
 
